879c042f202391475e17e36fc5ffd08e900199ab
Andrew <andrewyin1994@gmail.com>
2019-06-03 13:10:25 -0700

    to infinity and beyond

--- functionDrills2.js
+++ functionDrills2.js
@@ -1,6 +1,20 @@
-/* eslint-disable strict */
+/* eslint-disable no-console */
+'use strict';
+
 function jediName(firstName, lastName) {
   return lastName.slice(0,3) + firstName.slice(0,2);
 
 }
 console.log (jediName('Beyonce', 'Knowles'));
+
+function beyond(num) {
+  if(num === Infinity) console.log('And beyond.');
+  else if(num !== Infinity && num > 0) console.log('To infinity...');
+  else if(num !== Infinity && num < 0) console.log('To negative infinity...');
+  else console.log('Staying home.');
+}
+
+beyond(Infinity);
+beyond(1);
+beyond(-1);
+beyond(0);
--- test.js
+++
@@ -1,9 +0,0 @@
-let name;
-let age = 29;
-
-// if (name) 
-//   console.log(name);
-// else 
-//   console.log('nothing');
-
-console.log(name || age);
