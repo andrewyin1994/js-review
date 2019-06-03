b764f63acb432d78f47edf8367d0c8d7d3e79f16
Andrew <andrewyin1994@gmail.com>
2019-06-03 12:06:06 -0700

    added try catch for negative age

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
-function getYearOfBirth(age){
-  
+function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative.');
+  }
   return 2019 - age;
 }
+
 function createGreeting(name,age) {
-  const yearOfBirth=getYearOfBirth(29);
+  const yearOfBirth = getYearOfBirth(age);
+
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
-const greeting = createGreeting('Chris', 29);
-console.log(greeting);
+try {
+  const greeting = createGreeting('Chris', -1);
+  console.log(greeting);
+} catch(error) {
+  console.error(error);
+}
