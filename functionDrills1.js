18e1fad2a88ebc56d693ecead02e9ac8a0a9da2f
Mantong Zhao <mantong.cn@gmail.com>
2019-06-03 11:55:25 -0700

    add function getyob

--- functionDrills1.js
+++ functionDrills1.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
 /* eslint-disable strict */
-
+function getYearOfBirth(age){
+  
+  return 2019 - age;
+}
 function createGreeting(name,age) {
-  const yearOfBirth = 2019 - age;
+  const yearOfBirth=getYearOfBirth(29);
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
